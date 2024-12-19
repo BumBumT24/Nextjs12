a41218d25c9c79189f383e0dc2adfd81e1ce004f
Kacper Zawiszewski <60822138+BumBumT24@users.noreply.github.com>
2024-12-19 12:34:00 +0100

    next12 ostateczna wersja

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,27 @@
 import Header from '@/app/components/Header';
-
+import classes from './page.module.css';
+import MealsGrid from '../components/meals/meal-grid';
+import Link from 'next/link';
 export default function Home() {
     return (
       <main>
-        <h1 style={{ color: 'white', textAlign: 'center' }}>
-          Laboratorium nr 11 - Meals
-        </h1>
-        <Header title="Meals" subtitle="Witaj na stronie meals"/>
-
+        <header className={classes.header}>
+          <h1>
+            Delicious meals, created {' '}
+            <span classname={classes.highlight}>by you</span>
+          </h1>
+          <p>
+            Choose your favorite recipe and cook it yourself. It is easy and fun!
+          </p>
+          <p className={classes.cta}>
+            <Link href="/meals/share">
+            Share Your Favorite Recipe
+            </Link>
+          </p>
+        </header>
+        <main className={classes.main}>
+          
+        </main>
       </main>
     );
   }
